d48ae00d18c8fad6305fb4b21e3d3e56d484d895
Cyclic3 <cyclic3.git@gmail.com>
2019-01-19 19:49:03 +0000

    got cpack to be my friend too

--- cmake/c3-nu-config.cmake
+++
@@ -1,7 +0,0 @@
-# This is where it should be
-# TODO: actual logic
-
-find_path(c3-nu_INCLUDE_DIRS c3/upsilon)
-find_package_handle_standard_args(c3-upsilon  DEFAULT_MSG
-                                  c3-nu_INCLUDE_DIRS
-mark_as_advanced(c3-nu_INCLUDE_DIRS)
