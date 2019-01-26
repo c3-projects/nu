d8a5b847dc354a79ede9d795a7c202b70a21fd9d
Cyclic3 <cyclic3.git@gmail.com>
2019-01-26 18:11:19 +0000

    Added mapping to cancellable, and tidied up the API a bit

--- include/c3/nu/concurrency/cancellable.hpp
+++ include/c3/nu/concurrency/cancellable.hpp
@@ -19,6 +19,7 @@ namespace c3::nu {
   /// and all of its cancellable chidlren
   enum class cancellable_state {
     Provided,
+    PartiallyProvided,
     Cancelled,
     Undecided
   };
@@ -26,105 +27,114 @@ namespace c3::nu {
   template<typename T>
   class cancellable_provider;
 
+  template<typename T, typename Base>
+  class mapped_cancellable;
+
+  /// Taking the result of a completed cancellable is not thread safe
   template<typename T>
   class cancellable {
-    friend cancellable_provider<T>;
+    template<typename All>
+    friend class cancellable_provider;
+  public:
+    using provided_t = T;
 
   private:
     class shared_state_t {
     public:
-      std::optional<T> result = std::nullopt;
-
-      gateway_bool some_state_decided;
-      gateway_bool final_state_decided;
+      virtual gateway_bool& final_state_decided() = 0;
+      virtual const gateway_bool& final_state_decided() const = 0;
+      virtual gateway_bool& some_state_decided() = 0;
+      virtual const gateway_bool& some_state_decided() const = 0;
+      virtual std::optional<T> get_value() const = 0;
+      virtual std::optional<T> take_value() = 0;
+      virtual void set_value(T&&) = 0;
+      virtual bool has_value() const = 0;
 
     public:
-      bool is_cancelled() const {
-        return final_state_decided && !result.has_value();
+      inline bool is_cancelled() const {
+        return final_state_decided() && !has_value();
       }
 
       inline cancellable_state get_state() const {
-        if (!final_state_decided) return cancellable_state::Undecided;
-        if (result) return cancellable_state::Provided;
+        if (!final_state_decided()) {
+          if (some_state_decided())
+            return cancellable_state::PartiallyProvided;
+          else
+            return cancellable_state::Undecided;
+        }
+        if (has_value()) return cancellable_state::Provided;
         else return cancellable_state::Cancelled;
       }
     };
+    class simple_state;
+    template<typename Base>
+    class mapped_state;
 
-  private:
+  protected:
     std::shared_ptr<shared_state_t> shared_state;
 
   public:
-    /// Returns the result if it has been provided or cancelled, otherwise cancels the result
-    std::optional<T> get_or_cancel() {
-      // Cancel if not set
-      shared_state->final_state_decided.maybe_open([&] {
-        shared_state->some_state_decided.open();
+    /// Blocks until a result is provided or cancelled, and returns it
+    inline void wait() {
+      shared_state->some_state_decided().wait_for_open();
+    }
+    /// Blocks until a result is provided or cancelled, and returns it
+    inline bool wait(timeout_t timeout) {
+      return shared_state->some_state_decided().wait_for_open(timeout);
+    }
+
+    /// Blocks until a final result is provided or cancelled, and returns it
+    inline void wait_final() {
+      shared_state->final_state_decided().wait_for_open();
+    }
+    /// Blocks until a final result is provided or cancelled, and returns it
+    inline bool wait_final(timeout_t timeout) {
+      return shared_state->final_state_decided().wait_for_open(timeout);
+    }
+
+    inline void finalise() {
+      // Cancels if not set, finalises existing state
+      shared_state->final_state_decided().maybe_open([&] {
+        shared_state->some_state_decided().open();
         return true;
       });
-
-      return shared_state->result;
     }
 
-    /// Returns the result if it has been provided, (or std::nullopt if it was cancelled)
-    /// within the given timeout, otherwise cancels the result
-    std::optional<T> get_or_cancel(timeout_t timeout) {
+    inline bool finalise(timeout_t timeout) {
+      // Since this is lockless, we cannot reliable report whether the final data was a final provision
       // Wait for the final result
-      shared_state->final_state_decided.open_after(timeout);
+      auto ret = shared_state->final_state_decided().open_after(timeout);
       // This doesn't need to be locked,
-      // as all functions should check final_state_decicde first
-      shared_state->some_state_decided.open();
-      return shared_state->result;
-    }
-
-    /// Blocks until a result is provided or cancelled, and returns it
-    std::optional<T> wait() {
-      shared_state->some_state_decided.wait_for_open();
-      return shared_state->result;
-    }
-    /// Blocks until a result is provided or cancelled, and returns it
-    std::optional<T> wait(timeout_t timeout) {
-      shared_state->some_state_decided.wait_for_open(timeout);
-
-      std::optional<T> ret;
-
-      // Make sure we serialise the reads to result
-      shared_state->final_state_decided.maybe_open([&] {
-        if (shared_state->result) {
-          ret = shared_state->result;
-          return true;
-        }
-        else return false;
-      });
-
-      return ret;
-    }
-
-    /// Blocks until a final result is provided or cancelled, and returns it
-    std::optional<T> wait_final() {
-      shared_state->final_state_decided.wait_for_open();
-      return shared_state->result;
-    }
-    /// Blocks until a final result is provided or cancelled, and returns it
-    std::optional<T> wait_final(timeout_t timeout) {
-      shared_state->final_state_decided.wait_for_open(timeout);
-      std::optional<T> ret;
-      shared_state->final_state_decided.critical_section([&] {
-        ret = shared_state->result;
-      });
+      // as all functions should check final_state_decided first
+      shared_state->some_state_decided().open();
       return ret;
     }
 
     inline bool is_cancelled() { return shared_state->is_cancelled(); }
 
-    inline bool is_decided() { return shared_state->final_state_decided; }
+    inline bool is_decided() { return shared_state->final_state_decided(); }
+
+    inline cancellable_state get_state() const { return shared_state->get_state(); }
 
     /// Returns the result if it has been provided, otherwise returns std::nullopt
     inline std::optional<T> try_get() {
-      if (shared_state->final_state_decided)
-        return shared_state->result;
-      else if (shared_state->some_state_decided) {
-        shared_state->final_state_decided.open();
-        return shared_state->result;
+      if (shared_state->final_state_decided())
+        return shared_state->get_value();
+      else if (shared_state->some_state_decided()) {
+        shared_state->final_state_decided().open();
+        return shared_state->get_value();
+      }
+      else
+        return std::nullopt;
+    }
+
+    /// Takes the result if it has been provided, otherwise returns std::nullopt
+    inline std::optional<T> try_take() {
+      if (shared_state->final_state_decided())
+        return shared_state->take_value();
+      else if (shared_state->some_state_decided()) {
+        shared_state->final_state_decided().open();
+        return shared_state->take_value();
       }
       else
         return std::nullopt;
@@ -132,34 +142,136 @@ namespace c3::nu {
 
     /// Returns the final result if it has been provided, otherwise returns std::nullopt
     inline std::optional<T> try_get_final() {
-      if (shared_state->final_state_decided)
-        return  shared_state->result;
+      if (shared_state->final_state_decided())
+        return shared_state->get_value();
     }
 
-    inline cancellable_state get_state() const { return shared_state->get_state(); }
+    /// Takes the final result if it has been provided, otherwise returns std::nullopt
+    inline std::optional<T> try_take_final() {
+      if (shared_state->final_state_decided())
+        return shared_state->take_value();
+    }
+
+    /// Returns the result if it has been provided, otherwise returns std::nullopt
+    inline std::optional<T> try_get(timeout_t timeout) {
+      wait(timeout);
+      return try_get();
+    }
+
+    /// Takes the result if it has been provided, otherwise returns std::nullopt
+    inline std::optional<T> try_take(timeout_t timeout) {
+      wait(timeout);
+      return try_take();
+    }
+
+    /// Returns the final result if it has been provided, otherwise returns std::nullopt
+    inline std::optional<T> try_get_final(timeout_t timeout) {
+      wait_final(timeout);
+      return try_take();
+    }
+
+    /// Takes the final result if it has been provided, otherwise returns std::nullopt
+    inline std::optional<T> try_take_final(timeout_t timeout) {
+      wait_final(timeout);
+      return try_take();
+    }
+
+    template<typename Ret>
+    inline cancellable<Ret> map(std::function<Ret(T)> func) {
+      return {std::make_shared<mapped_state<Ret>>(shared_state, std::move(func))};
+    }
 
   private:
     inline cancellable(decltype(shared_state) shared_state) : shared_state{shared_state} {}
   };
 
   template<typename T>
-  class cancellable_provider {
+  class cancellable<T>::simple_state final : public cancellable<T>::shared_state_t {
   private:
-    std::shared_ptr<typename cancellable<T>::shared_state_t> shared_state =
-      std::make_shared<typename cancellable<T>::shared_state_t>();
+    std::optional<T> result = std::nullopt;
+
+    gateway_bool _some_state_decided;
+    gateway_bool _final_state_decided;
 
   public:
-    template<typename Func>
-    inline cancellable_state maybe_provide(Func func) {
+    inline gateway_bool& final_state_decided() override { return _final_state_decided; }
+    inline const gateway_bool& final_state_decided() const override { return _final_state_decided; }
+    inline const gateway_bool& some_state_decided() const override { return _some_state_decided; }
+    inline gateway_bool& some_state_decided() override { return _some_state_decided; }
+    inline std::optional<T> get_value() const override {
+      return result;
+    }
+    inline std::optional<T> take_value() override {
+      if (result) {
+        std::optional<T> ret = std::move(*result);
+        result.reset();
+        return ret;
+      }
+      return {};
+    }
+    inline void set_value(T&& value) override {
+      result = std::move(value);
+    }
+    inline bool has_value() const override { return result.has_value(); }
+  };
+
+  /// Acts both as a mapper for set, or a mapper for get
+  template<typename T>
+  template<typename Base>
+  class cancellable<T>::mapped_state final : public cancellable<T>::shared_state_t {
+  private:
+    std::shared_ptr<typename cancellable<Base>::shared_state_t> base;
+    std::function<T(Base)> mapper;
+
+  public:
+    inline gateway_bool& final_state_decided() override { return base->final_state_decided(); }
+    inline gateway_bool& some_state_decided() override { return base->some_state_decided(); }
+    inline const gateway_bool& final_state_decided() const override { return base->final_state_decided(); }
+    inline const gateway_bool& some_state_decided() const override { return base->some_state_decided(); }
+    inline std::optional<T> get_value() const override {
+      if (auto value = base->get_value())
+        return { mapper(*value) };
+      else
+        return std::nullopt;
+    }
+    inline std::optional<T> take_value() override {
+      if (auto value = base->take_value())
+        return { mapper(*value) };
+      else
+        return std::nullopt;
+    }
+    inline void set_value(T&&) override {
+      throw std::logic_error("cancellable<T>::mapped_state used to set value");
+    }
+    inline bool has_value() const override { return base->has_value(); }
+
+  public:
+    mapped_state(decltype(base) base, decltype(mapper) mapper) : base{base}, mapper{mapper} {}
+  };
+
+  template<typename T>
+  class cancellable_provider {
+  public:
+    using provided_t = T;
+
+  private:
+    template<typename Base>
+    class mapped_state;
+
+  private:
+    std::shared_ptr<typename cancellable<T>::shared_state_t> shared_state;
+
+  public:
+    inline cancellable_state maybe_provide(std::function<std::optional<T>()> func) {
       cancellable_state ret;
 
-      shared_state->final_state_decided.maybe_open([&] {
+      shared_state->final_state_decided().maybe_open([&] {
         try {
           std::optional<T> result = func();
 
           if (result) {
-            shared_state->result = std::move(result);
-            shared_state->some_state_decided.open();
+            shared_state->set_value(std::move(*result));
+            shared_state->some_state_decided().open();
             ret = cancellable_state::Provided;
 
             return true;
@@ -170,7 +282,7 @@ namespace c3::nu {
           }
         } catch(...) {
           ret = cancellable_state::Cancelled;
-          shared_state->some_state_decided.open();
+          shared_state->some_state_decided().open();
           return true;
         }
       });
@@ -185,14 +297,14 @@ namespace c3::nu {
     inline cancellable_state maybe_update(Func func) {
       cancellable_state ret;
 
-      shared_state->final_state_decided.maybe_open([&] {
+      shared_state->final_state_decided().maybe_open([&] {
         try {
           std::optional<T> result = func();
 
           if (result) {
-            shared_state->result = std::move(result);
-            shared_state->some_state_decided.open();
-            ret = cancellable_state::Provided;
+            shared_state->set_value(std::move(*result));
+            shared_state->some_state_decided().open();
+            ret = cancellable_state::PartiallyProvided;
 
             return false;
           }
@@ -202,9 +314,15 @@ namespace c3::nu {
           }
         } catch(...) {
           ret = cancellable_state::Cancelled;
-          shared_state->some_state_decided.open();
+          shared_state->some_state_decided().open();
           return true;
         }
+      },
+      [&] {
+        if (shared_state->has_value())
+          ret = cancellable_state::Provided;
+        else
+          ret = cancellable_state::Cancelled;
       });
 
       return ret;
@@ -212,13 +330,50 @@ namespace c3::nu {
 
     inline bool is_cancelled() { return shared_state->is_cancelled(); }
 
-    inline bool is_decided() { return shared_state->final_state_decided; }
+    inline bool is_decided() { return shared_state->final_state_decided(); }
 
     inline cancellable_state get_state() const { return shared_state->get_state(); }
 
     inline void cancel() { shared_state->final_state_decided.open(); }
 
+    template<typename Ret>
+    inline cancellable_provider<Ret> map(std::function<T(Ret)> func) {
+      return {std::make_shared<mapped_state<Ret>>(shared_state, func)};
+    }
+
   public:
     inline cancellable<T> get_cancellable() { return { shared_state }; }
+
+  public:
+    cancellable_provider() : shared_state{std::make_shared<typename cancellable<T>::simple_state>()} {}
+    cancellable_provider(decltype(shared_state) shared_state) : shared_state{shared_state} {}
+  };
+
+  /// Acts both as a mapper for set, or a mapper for get
+  template<typename Base>
+  template<typename T>
+  class cancellable_provider<Base>::mapped_state final : public cancellable<T>::shared_state_t {
+  private:
+    std::shared_ptr<typename cancellable<Base>::shared_state_t> base;
+    std::function<Base(T)> mapper;
+
+  public:
+    inline gateway_bool& final_state_decided() override { return base->final_state_decided(); }
+    inline gateway_bool& some_state_decided() override { return base->some_state_decided(); }
+    inline const gateway_bool& final_state_decided() const override { return base->final_state_decided(); }
+    inline const gateway_bool& some_state_decided() const override { return base->some_state_decided(); }
+    inline std::optional<T> get_value() const override {
+      throw std::logic_error("cancellable_provider<T>::mapped_state used to set value");
+    }
+    inline std::optional<T> take_value() override {
+      throw std::logic_error("cancellable_provider<T>::mapped_state used to set value");
+    }
+    inline void set_value(T&& value) override {
+      base->set_value(mapper(value));
+    }
+    inline bool has_value() const override { return base->has_value(); }
+
+  public:
+    mapped_state(decltype(base) base, decltype(mapper) mapper) : base{base}, mapper{mapper} {}
   };
 }
