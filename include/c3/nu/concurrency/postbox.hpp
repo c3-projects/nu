bc5cede91d5e764f9042fdfe95cdb83903fb60a2
Cyclic3 <cyclic3.git@gmail.com>
2019-02-08 13:20:50 +0000

    Postbox is now pretty much done

--- include/c3/nu/concurrency/postbox.hpp
+++ include/c3/nu/concurrency/postbox.hpp
@@ -10,6 +10,13 @@ namespace c3::nu {
   template<typename T>
   class letterbox;
 
+  template<typename T>
+  class _postbox_manager {
+  public:
+    _postbox_manager(const _postbox_manager&) = delete;
+    _postbox_manager& operator=(const _postbox_manager&) = delete;
+  };
+
   template<typename Id, typename Value>
   class postbox {
     //friend class letterbox<Value>;
@@ -17,28 +24,48 @@ namespace c3::nu {
     worm_mutexed<std::map<Id, std::shared_ptr<letterbox<Value>>>> _boxes;
 
   public:
-    std::shared_ptr<letterbox<Value>> allocate(Id id) {
+    inline std::shared_ptr<letterbox<Value>> get(Id id) {
       auto handle = _boxes.get_rw();
-      handle.empace(id, std::make_shared<letterbox<Value>>());
+      auto iter = handle->find(id);
+      if (iter == handle->end())
+        return handle->emplace(id, std::make_shared<letterbox<Value>>());
+      else
+        return *iter;
     }
-  };
+    inline void remove(Id id) {
+      auto handle = _boxes.get_rw();
+      handle.erase(id);
+    }
+  public:
+    inline void post(Id id, Value v) {
+      get(id)->post(v);
+    }
 
+  public:
+    inline std::shared_ptr<letterbox<Value>> operator[](Id id) { return get(id); }
+  };
 
   template<typename T>
   class letterbox {
   private:
     concurrent_queue<T> _box;
   public:
-    void post(T t) {
+    inline void post(T t) {
       _box.push(std::move(t));
     }
-    T collect_one() {
+
+    inline T collect_one() {
       return _box.pop();
     }
-    /*
-    std::vector<T> collect() {
+
+    inline std::vector<T> collect_all() {
       std::vector<T> ret;
-      while(_box.pop());
-    }*/
+      while (auto x = _box.try_pop())
+        ret.emplace(std::move(x));
+    }
+
+    inline size_t n_to_collect() const {
+      return _box.size();
+    }
   };
 }
