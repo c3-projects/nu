6d57f1db4c8975a5871aa83dbd8605b107625e35
Cyclic3 <cyclic3.git@gmail.com>
2019-02-21 22:12:49 +0000

    Hex!

---
+++ tests/hex.cxx
@@ -0,0 +1,17 @@
+#include "c3/nu/data/hex.hpp"
+#include "c3/nu/data.hpp"
+
+using namespace c3::nu;
+
+#include <iostream>
+
+int main() {
+  std::string str = "Hello, world!";
+  std::string hex = hex_encode(str);
+  std::string str_ = hex_decode<std::string>(hex);
+
+  if (str != str_)
+    throw std::runtime_error("Base64 data corrupted!");
+
+  return 0;
+}
