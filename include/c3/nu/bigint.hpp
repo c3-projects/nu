202a594cbd5776d31f2a6ca3ed72bc566fb55154
Cyclic3 <cyclic3.git@gmail.com>
2019-03-04 08:51:51 +0000

    Started work on signed bigint

--- include/c3/nu/bigint.hpp
+++ include/c3/nu/bigint.hpp
@@ -6,31 +6,13 @@
 #include "c3/nu/int_maths.hpp"
 
 namespace c3::nu {
-  class biguint {
+  class bigint {
   private:
     std::vector<uint8_t> _data;
+    bool _sign;
 
-  public:
-    template<typename T>
-    constexpr bool can_convert() const noexcept {
-      return serialised_size<T>() >= _data.size();
-    }
-    template<>
-    constexpr bool can_convert<float>() const noexcept {
-      return true;
-    }
-    template<>
-    constexpr bool can_convert<double>() const noexcept {
-      return true;
-    }
-
-    inline void clean() {
-      while (_data.size() > 0 && _data.back() == 0)
-        _data.pop_back();
-    }
-
-  public:
-    inline biguint& operator+=(const biguint& other) {
+  private:
+    inline void _add_op(const bigint& other) {
       _data.resize(std::max(_data.size(), other._data.size()), 0);
 
       uint8_t carry = false;
@@ -48,18 +30,16 @@ namespace c3::nu {
 
       if (carry)
         _data.push_back(1);
-
-      return *this;
     }
 
-    /// XXX: UB if other > this
-    inline biguint& operator-=(const biguint& other) {
-
+    inline void _sub_op(const bigint& other) {
       uint8_t carry = false;
+
+      _data.resize(std::max(_data.size(), other._data.size()), 0);
 
       auto sub_start = _data.rbegin() + (_data.size() - other._data.size());
       auto a = sub_start;
-      for (auto b = other._data.rbegin(); a < _data.rend(); ++a, ++b) {
+      for (auto b = other._data.rbegin(); a != _data.rend(); ++a, ++b) {
         // Unsigned overflow is well-defined in c++, so we do not need to worry about it
         uint8_t old_carry = carry;
 
@@ -73,34 +53,62 @@ namespace c3::nu {
         *--sub_start -= 1;
 
       clean();
+    }
+
+  public:
+    template<typename T>
+    constexpr bool can_convert() const noexcept {
+      return serialised_size<T>() >= _data.size();
+    }
+
+    inline void clean() {
+      while (_data.size() > 0 && _data.back() == 0)
+        _data.pop_back();
+    }
+
+  public:
+    inline bigint& operator+=(const bigint& other) {
+      if (other._sign == _sign)
+        _add_op(other);
+      else
+        _sub_op(other);
 
       return *this;
     }
 
-    inline biguint operator+(const biguint& other) const {
-      biguint clone = *this;
+    inline bigint& operator-=(const bigint& other) {
+      if (other._sign != _sign)
+        _add_op(other);
+      else
+        _sub_op(other);
+
+      return *this;
+    }
+
+    inline bigint operator+(const bigint& other) const {
+      bigint clone = *this;
       clone += other;
       return clone;
     }
-    inline biguint operator-(biguint other) const {
-      biguint clone = *this;
+    inline bigint operator-(bigint other) const {
+      bigint clone = *this;
       clone -= other;
       return clone;
     }
 
   public:
-    constexpr bool operator<(const biguint& other) const {
+    constexpr bool operator<(const bigint& other) const {
       if (_data.size() != other._data.size())
         return _data.size() < other._data.size();
       else {
-        for (auto a = _data.rbegin(), b = other._data.rbegin(); a < _data.rend(); ++a, ++b) {
+        for (auto a = _data.rbegin(), b = other._data.rbegin(); a != _data.rend(); ++a, ++b) {
           if (a == b) continue;
           else return a < b;
         }
       }
       return false;
     }
-    constexpr bool operator>(const biguint& other) const {
+    constexpr bool operator>(const bigint& other) const {
       if (_data.size() != other._data.size())
         return _data.size() > other._data.size();
       else {
@@ -111,15 +119,15 @@ namespace c3::nu {
       }
       return false;
     }
-    constexpr bool operator>=(const biguint& other) const { return !(*this < other); }
-    constexpr bool operator<=(const biguint& other) const { return !(*this > other); }
-    inline bool operator==(const biguint& other) const { return _data == other._data; }
+    constexpr bool operator>=(const bigint& other) const { return !(*this < other); }
+    constexpr bool operator<=(const bigint& other) const { return !(*this > other); }
+    inline bool operator==(const bigint& other) const { return _data == other._data; }
 
   public:
     template<typename T, typename = typename std::is_integral<T>::type>
     inline explicit operator T() const {
       if (!can_convert<T>())
-        throw std::runtime_error("Cannot fit biguint in requested type");
+        throw std::runtime_error("Cannot fit bigint in requested type");
 
       nu::data be_data(serialised_size<T>());
       std::copy(_data.rbegin(), _data.rend(),
@@ -129,10 +137,20 @@ namespace c3::nu {
 
   public:
     template<typename T, typename = typename std::is_integral<T>::type>
-    inline biguint(T t) : _data(serialised_size<T>()) {
+    inline bigint(T t) : _data(serialised_size<T>()) {
       nu::data be_data = serialise(t);
       std::copy(be_data.rbegin(), be_data.rend(), _data.begin());
       clean();
     }
   };
+
+
+  template<>
+  constexpr bool bigint::can_convert<float>() const noexcept {
+    return true;
+  }
+  template<>
+  constexpr bool bigint::can_convert<double>() const noexcept {
+    return true;
+  }
 }
