06d3cbecd6a67d00cc3c880dbc41ef03fb69378b
Cyclic3 <cyclic3.git@gmail.com>
2019-03-12 12:45:06 +0000

    Finished safe iter

--- include/c3/nu/safe_iter.hpp
+++ include/c3/nu/safe_iter.hpp
@@ -1,82 +1,117 @@
 #pragma once
 
 #include <iterator>
+#include "c3/nu/moveable_ptr.hpp"
 
 namespace c3::nu {
-  template<typename Iter, typename IterType = typename std::iterator_traits<Iter>::iterator_category>
-  class safe_iter;
 
   template<typename Iter>
-  class safe_iter_base {
-    template<typename Iter, typename IterType>
-    friend class safe_iter;
+  class safe_iter {
+  private:
+    using traits = std::iterator_traits<Iter>;
+
+  public:
+    using diffence_type = typename traits::difference_type;
+    using value_type = typename traits::value_type;
+    using pointer = typename traits::pointer;
+    using reference = typename traits::reference;
+    using iterator_category = typename std::forward_iterator_tag;
 
   private:
     Iter base;
     Iter end;
 
+  private:
+    inline void incr() {
+      if (base != end)
+        ++base;
+    }
+
   public:
-    inline void operator++() {
-      if (base == end) throw std::out_of_range("Tried to iterate past end");
-      ++base;
+    inline safe_iter operator++() {
+      incr();
       return *this;
     }
-    inline decltype(auto) operator*() { return *base; }
+    inline safe_iter operator++(int) {
+      auto cpy = *this;
+      incr();
+      return cpy;
+    }
+    inline typename traits::difference_type operator-(const safe_iter& other) {
+      return base - other.base;
+    }
+
+    inline safe_iter& operator+=(typename traits::difference_type diff) {
+      if (diff < 0)
+        throw std::runtime_error("Cannot decrement safe iterators");
+
+      if constexpr (std::is_same_v<traits::iterator_category, std::random_access_iterator_tag>) {
+        auto dist_from_end = end - base;
+        if (diff > dist_from_end)
+          base = end;
+        else
+          base += dist_from_end;
+      }
+      else {
+        while (diff > 0 && base != end)
+          incr();
+      }
+      return *this;
+    }
+
+    inline safe_iter operator[](typename traits::difference_type diff) {
+      auto cpy = *this;
+      return this += diff;
+    }
 
   public:
-    inline bool operator==(const safe_iter_base& other) const { return base == other.base; }
-    inline bool operator!=(const safe_iter_base& other) const { return base != other.base; }
+    inline typename traits::reference operator*() {
+      if (base == end)
+        throw std::range_error("Tried to deref safe iterator out of range");
+      else
+        return *base;
+    }
+    inline typename traits::reference operator->() {
+      if (base == end)
+        throw std::range_error("Tried to deref safe iterator out of range");
+      else
+        return base.operator->();
+    }
+
+  public:
+    inline bool operator==(const safe_iter& other) const { return base == other.base; }
+    inline bool operator!=(const safe_iter& other) const { return base != other.base; }
+    inline bool operator<=(const safe_iter& other) const { return base <= other.base; }
+    inline bool operator>=(const safe_iter& other) const { return base >= other.base; }
+    inline bool operator< (const safe_iter& other) const { return base <  other.base; }
+    inline bool operator> (const safe_iter& other) const { return base >  other.base; }
+
+  public:
+    inline bool is_end() { return base == end; }
+
+  public:
+    inline safe_iter(Iter base, Iter end) : base{base}, end{end} {}
+    inline safe_iter() = default;
   };
 
-  template<typename Iter>
-  class safe_iter<Iter, std::input_iterator_tag> : public safe_iter_base<Iter> {
+  template<typename T>
+  class safe {
   public:
-    using safe_iter_base<Iter>::operator++;
-  public:
-    using safe_iter_base<Iter>::operator==;
-    using safe_iter_base<Iter>::operator!=;
+    using iterator = safe_iter<typename T::iterator>;
+    using const_iterator = safe_iter<typename T::const_iterator>;
+
+  private:
+    moveable_ptr<T> base = nullptr;
 
   public:
-    safe_iter(Iter base, Iter end) : safe_iter_base<Iter>(base, end) {}
-  };
-  template<typename Iter>
-  class safe_iter<Iter, std::output_iterator_tag> : public safe_iter_base<Iter> {
-  public:
-    using safe_iter_base<Iter>::operator++;
-  public:
-    using safe_iter_base<Iter>::operator==;
-    using safe_iter_base<Iter>::operator!=;
+    iterator begin() { return { base->begin(), base->end() }; }
+    iterator end() { return { base->end(), base->end() }; }
+    iterator begin() const { return { base->begin(), base->end() }; }
+    iterator end() const { return { base->end(), base->end() }; }
+    const_iterator cbegin() const { return { base->cbegin(), base->cend() }; }
+    const_iterator cend() const { return { base->cend(), base->cend() }; }
 
   public:
-    safe_iter(Iter base, Iter end) : safe_iter_base<Iter>(base, end) {}
-  };
-
-  template<typename Iter>
-  class safe_iter<Iter, std::forward_iterator_tag> {
-  public:
-    using safe_iter_base<Iter>::operator++;
-  public:
-    using safe_iter_base<Iter>::operator==;
-    using safe_iter_base<Iter>::operator!=;
-
-  public:
-    safe_iter(Iter base, Iter end) : safe_iter_base<Iter>(base, end) {}
-    safe_iter() {}
-  };
-
-  template<typename Iter>
-  class safe_iter<Iter, std::bidirectional_iterator_tag> :
-      public safe_iter<Iter, std::forward_iterator_tag> {
-  public:
-    using safe_iter_base<Iter>::operator==;
-    using safe_iter_base<Iter>::operator!=;
-  };
-
-  template<typename Iter>
-  class safe_iter<Iter, std::random_access_iterator_tag> :
-      public safe_iter<Iter, std::bidirectional_iterator_tag> {
-  public:
-    using safe_iter_base<Iter>::operator==;
-    using safe_iter_base<Iter>::operator!=;
+    inline safe(T& t) : base{&t} {}
   };
 }
---
+++ tests/safe_iter.cxx
@@ -0,0 +1,37 @@
+#include "c3/nu/safe_iter.hpp"
+
+#include <vector>
+
+using namespace c3::nu;
+
+int main() {
+  std::vector<int> yeet {1,2};
+
+  auto begin = safe(yeet).begin();
+
+  if (begin.is_end())
+    throw std::runtime_error("safe iter set to end at start");
+
+  if ((++begin).is_end())
+    throw std::runtime_error("safe iter set to end after first incr");
+
+  if (!(++begin).is_end())
+    throw std::runtime_error("safe iter not set to end after second incr");
+
+  try {
+    *begin;
+    throw std::runtime_error("Dereferencing at end did not throw an exception");
+  }
+  catch (std::range_error) {}
+
+  ++begin;
+
+  try {
+    *begin;
+    throw std::runtime_error("Dereferencing after end did not throw an exception");
+  }
+  catch (std::range_error) {}
+
+  return 0;
+}
+
