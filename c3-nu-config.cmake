5d85c88b682351cf1979779acc9d7098d54e883d
Cyclic3 <cyclic3.git@gmail.com>
2019-03-21 13:13:37 +0000

    I dislike osx

--- c3-nu-config.cmake
+++ c3-nu-config.cmake
@@ -2,5 +2,5 @@
 add_library(c3-nu INTERFACE IMPORTED GLOBAL)
 set_target_properties(c3-nu PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_CURRENT_LIST_DIR}/include)
 
-
-
+find_path (GSL_INCLUDE_DIR NAMES span gsl)
+include_directories(${GSL_INCLUDE_DIR})
